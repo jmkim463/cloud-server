73a64ba47e24e100e5aaa3b98516fc3942d3a348
jmkim463 <126214989+jmkim463@users.noreply.github.com>
2023-09-07 10:43:59 +0900

    ㅅㅈ

--- deploy.sh
+++ deploy.sh
@@ -19,4 +19,4 @@ fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/action/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/action/deploy_err.log &
+nohup java -cp $DEPLOY_PATH -jar $DEPLOY_JAR >> /home/ec2-user/deploy.alog 2>/home/ec2-user/action/deploy_err.log &
